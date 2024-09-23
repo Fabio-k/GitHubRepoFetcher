058b57de8993b076198260d7fa46b596c3e85176
fabio-k <fabiokmain@gmail.com>
2024-09-23 14:21:03 -0300

    remove the html interface

--- app/controllers/homepage_controller.rb
+++
@@ -1,9 +0,0 @@
-class HomepageController < ApplicationController
-  def index
-    @response = GithubApi.get_all_repositories
-  end
-
-  def show
-    @repository = GithubApi.get_repository(params[:name])
-  end
-end
